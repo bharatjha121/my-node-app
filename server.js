157ddc8c967ddcfd77f713ed67cf7bba8ae271aa
Bharat <bharatjha121@gmail.com>
2026-05-28 09:25:34 +0000

    v2: updated message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from EC2! GitHub Actions Deploy working! 🚀\n');
+  res.end('Version 2.0 - Auto Deployed via Github Action!');
 });
 
 server.listen(PORT, () => {
